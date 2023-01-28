c5d69a88594ea1b4b791d764f8735cc7a743e3c2
Trio Nugroho <75536764+ntrioooo@users.noreply.github.com>
2023-01-28 14:39:25 +0700

    change server

--- src/actions/formAction.js
+++ src/actions/formAction.js
@@ -17,7 +17,7 @@ export const postIsiDetail = (data) => (dispatch) => {
       // POST API
   axios({
     method: 'POST',
-    url: 'https://testdev5-production.up.railway.app/booking-ticket',
+    url: 'https://be-final-production.up.railway.app/booking-ticket',
     // url: 'http://localhost:8000/booking-ticket',
     timeout: 120000,
     data: data,
@@ -60,7 +60,7 @@ export const getIsiDetail = () => (dispatch) => {
   axios({
     method: 'GET',
     // url: 'http://localhost:8000/history',
-    url: 'https://testdev5-production.up.railway.app/history',
+    url: 'https://be-final-production.up.railway.app/history',
     timeout: 120000,
   })
     .then((response) => {
